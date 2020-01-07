25ad92a8c1c65a9273686eaa4ba0c08f8b5b9bd3
Rick Companje <rick@companje.nl>
2020-01-07 22:45:34 +0100

    improve parser

--- index.js
+++ index.js
@@ -11,18 +11,21 @@ var items=[];
 const liner = new lineByLine(filename);
 let line;
 var i=0;
+var separator = "(1) = ";
+var prevKey;
 
-var allTypes = ["winverl","adrs","aff","afk","afkb","avit","ambr","abk2","ads","abk","avrf","avr","avrp","aov","art","boekart","tsart","semtattr","aud","adab","audittr","bkt","bgin","bhmd","behtyp","bstk","bstpln","bcs","bidp","bm","bozk","boek","boekf","bdpn","bbrf","bd","btek","bouwverg","brf","brfh","bvg","cdev","categ","cm","col","rfco","dagb","db","digibest","digidoc","digimap","digiontv","dpm","dpin","dos","doss","dummy","aktve","ebr","ecpd","eb","akterk","fwpn","film","ff","fmat","ft","fotom","fncr","fob","geb","gdt","gelband","gf","gdop","genbr","grak","grvak","scnni","grsl","hwv","hsk","huw","ib","ill","ipob","ipot","inio","iiral","iirvo","inis","intvw","inv","krt","km","kdart","kdeig","kdad","kvg","krant","krtp","lic","insldmt","lst","lici","lozverg","mvs","meta","mf","milverg","mnmnt","mnt","nwg","nt","ntni","notakt","not","objt","obja","objafb","octv","ogvg","otreg","ozgs","ozgbes","ozgozt","ozghsk","ozglnk","ozgops","ozgth","ozgtr","ozgtst","digiarch","digiontvls","ordon","ove","ovevk","pgblr","pibr","pmz","pin","pgpsr","pgpr","pipr","pgpb","pft","pap","pgf","vw_prm","vwdef_par","vwmdl_prm","pglst","pen","perc","pdos","psa","pbelr","pbr","piib","piit","psms","psm","pnna","pior","pspsr","pspr","pepr","pspb","psnpd","piral","pirvo","piwm","psv","psafb","poib","pb","pskrt","pzgl","prent","pbk","pd","proj","prac","prbt","prps","rks","reg","rvwm","rmp","rub","scn","scnt","scat","smld","shsrt","bhst","btgn","sa","selreg","semtag","srafb","sboek","stijd","siso","esjb","sloopdos","vw_stp","vwmdl_stp","strn","skz","tbstr","ttek","tng","tst","ts","semtatval","trck","twin","udc","c_uitvrm","verg","vdr","vwk","vwdef_stp","vwmdl","vwkarch","zie","vb","vdb","vid","vngrub","von","vw","wrkst","wksttm","wnkrt","zgl"]
+var allTypes = ["winverl","adrs","aff","afk","afkb","avit","ambr","abk2","ads","abk","avrf","avr","avrp","aov","art","boekart","tsart","semtattr","aud","adab","audittr","bkt","bgin","bhmd","behtyp","bstk","bstpln","bcs","bidp","bm","bozk","boek","boekf","bdpn","bbrf","bd","btek","bouwverg","brf","brfh","bvg","cdev","categ","cm","col","rfco","dagb","db","digibest","digidoc","digimap","digiontv","dpm","dpin","dos","doss","dummy","aktve","ebr","ecpd","eb","akterk","fwpn","film","ff","fmat","ft","fotom","fncr","fob","geb","gdt","gelband","gf","gdop","genbr","grak","grvak","scnni","grsl","hwv","hsk","huw","ib","ill","ipob","ipot","inio","iiral","iirvo","inis","intvw","inv","krt","km","kdart","kdeig","kdad","kvg","krant","krtp","lic","insldmt","lst","lici","lozverg","mvs","meta","mf","milverg","mnmnt","mnt","nwg","nt","ntni","notakt","not","objt","obja","objafb","octv","ogvg","otreg","ozgs","ozgbes","ozgozt","ozghsk","ozglnk","ozgops","ozgth","ozgtr","ozgtst","digiarch","digiontvls","ordon","ove","ovevk","pgblr","pibr","pmz","pin","pgpsr","pgpr","pipr","pgpb","pft","pap","pgf","vw_prm","vwdef_par","vwmdl_prm","pglst","pen","perc","pdos","psa","pbelr","pbr","piib","piit","psms","psm","pnna","pior","pspsr","pspr","pepr","pspb","psnpd","piral","pirvo","piwm","psv","psafb","poib","pb","pskrt","pzgl","prent","pbk","pd","proj","prac","prbt","prps","rks","reg","rvwm","rmp","rub","scn","scnt","scat","smld","shsrt","bhst","btgn","sa","selreg","semtag","srafb","sboek","stijd","siso","esjb","sloopdos","vw_stp","vwmdl_stp","strn","skz","tbstr","ttek","tng","tst","ts","semtatval","trck","twin","udc","c_uitvrm","verg","vdr","vwk","vwdef_stp","vwmdl","vwkarch","zie","vb","vdb","vid","vngrub","von","vw","wrkst","wksttm","wnkrt","zgl","inl"]
 
 var output = [];
 
 console.log('[');
 while (line = liner.next()) {
     const str = line.toString();
-    const key = str.substr(0,str.indexOf("=")-4);
-    const val = str.substr(str.indexOf("=")+2);
+    const key = str.substr(0,str.indexOf(separator));
+    const val = str.substr(str.indexOf(separator)+separator.length);
 
-    if (key=="%0") item=null;
+    if (key=="") updateItem(prevKey,str); //multi-line
+    else if (key=="%0") item=null;
     else if (allTypes.indexOf(key)>-1) nextItem(key,val);
     else updateItem(key,val);
 }
@@ -30,14 +33,14 @@ console.log(']');
 
 function nextItem(key,val) {
     if (item) saveItem(); //save if there's currently an item being parsed (not header)
-    item = { recordType: key, recordID: parseInt(val) }; //create a new item
+    item = { recordType: key } //, recordID: parseInt(val) }; //create a new item
     updateItem(key,val); //add identifier such as ft=123 or krt=5
 }
 
 function saveItem() {
     // console.log(item);
-    if (item) console.log(JSON.stringify(item,null,4) + ",");
-
+    if (i++>0) console.log(",");
+    if (item) console.log(JSON.stringify(item,null,4));
 }
 
 function updateItem(key,val) {
@@ -49,5 +52,6 @@ function updateItem(key,val) {
           item[key].push(value);
         } 
     }
+    prevKey = key;
 }
 
